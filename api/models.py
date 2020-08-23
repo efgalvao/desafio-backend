88a152f35a24980b1dbaedd8b89295a34a75a7dc
efgalvao <solfieri@hotmail.com>
2020-08-23 09:19:00 -0300

    Classes criadas

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Classificacao(models.Model):
+    classificacao = models.CharField("Classificação", max_length=20)
+
+    def __str__(self):
+        return self.classificacao
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Classificação'
+        verbose_name_plural = 'Classificações'
+
+class Viagem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    data_inicio = models.DateTimeField("Início")
+    data_fim = models.DateTimeField("Fim")
+    classificacao = models.ForeignKey(Classificacao, on_delete=models.CASCADE, blank=True, null=True)
+    nota = models.IntegerField("Nota", validators=[MaxValueValidator, MinValueValidator],
+                               blank=True, null=True)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Viagem'
+        verbose_name_plural = 'Viagens'
